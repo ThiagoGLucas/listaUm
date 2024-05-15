918f8a35d1a370a42692ff083978d83d9ea1af17
ThiagoGLucas <168426420+ThiagoGLucas@users.noreply.github.com>
2024-05-15 12:01:08 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+	private int somar(int a, int b) {
+		return a + b;
+	}
+	public int subtrair(int a, int b) {
+		return a - b;
+	}
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+	public int dividir(int a, int b) {
+		return a / b;
+	}
+}
